1816c8353b39a8d60593aad9ab55f891079fe9bf
Alejandro <eloso85@gmail.com>
2021-03-06 15:20:26 -0600

    making things work

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,60 @@
-$(()=>{
-    
-    var taskValue = $("#task-value");
-    var taskList = $("#task-list");
-    
-   var todo = []
-   getTodos()
+$( document ).on( "click",".delete", deleteTodo);
+$( document ).on( "click", ".edit", editTodo )
+$( document ).on("input", "listItem", inputListen)
 
-   function initializeRows(){
-       taskList.empty();
-       var rowsToAdd = [];
-       for (var i = 0 ; i< todo.length; i++ ){
-           rowsToAdd.push(createNewRow(todo[i]));
-       }
-       taskList.prepend(rowsToAdd);
-   }
-
+getTodos();
+   
    function getTodos(){
        $.get("/api/task", data=>{
-           todo = data;
-           console.log(todo)
-           initializeRows()
+        console.log(data)
+           $("#task-list").html(`${data.map((todo)=>{
+               //console.log(todo.text)
+               return`<li class="listItem" id=${todo.id} contenteditable="true" >${todo.text}</li><button class='delete' type="button"  id=${todo.id}  >delete</button><button class="edit" id=${todo.id}>edit</button>`
+           }).join("")}`);
+          
+           
        });
-   }
-   function createNewRow(todo) {
-    var $newInputRow = $(
-      [
-        "<li class='list-group-item todo-item'>",
-        "<span>",
-        todo.text,
-        "</span>",
-        "<input type='text' class='edit' style='display: none;'>",
-        "<button class='delete btn btn-danger'>x</button>",
-        "<button class='complete btn btn-primary'>✓</button>",
-        "</li>"
-      ].join("")
-    );
-   return $newInputRow
+   };
 
-    }
+   function inputListen(){
+       console.log("input fired")
+   }
+
+   function editTodo(){
+       
+       var id = $(this).attr("id")
+       console.log(id)
+       return $(`#${id}`).html( `<li class="listItem"  contenteditable="true" ></li`)
+       
+       
+   }
+
+   function deleteTodo(event) {
+    event.stopPropagation();
+    var id = $(this).attr("id")
+    $.ajax({
+        method: "DELETE",
+        url: "/api/task/" + id
+      }).then(getTodos);
+    
+   console.log(id)
+  }
+  
+
+   $("#submit").on("click",(event)=>{
+       event.preventDefault();
+       var todo = {
+           text:$("#task-value").val().trim(),
+            complete: false
+        };
+        console.log(todo)
+        $.post("/api/task",todo,getTodos);
+        $("#task-value").val("");
+   });
+
+  
+
+
+
    
     
-})
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,5 +9,49 @@ module.exports = app =>{
         .then(function(dbTodo){
             res.json(dbTodo)
         })
-    })
-}
+    });
+
+    app
+    .post("/api/task", function(req, res){
+        console.log(req.body)
+
+        db
+        .Task
+        .create({
+            text: req.body.text,
+            complete: req.body.complete
+        }).then(function(dbTodo){
+            res.json(dbTodo)
+        });
+    });
+
+    app
+    .delete("/api/task/:id", function(req,res){
+        db
+        .Task
+        .destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbTodo){
+            res.json(dbTodo)
+        });
+    });
+    app
+.put("/api/task", function(req, res){
+    db
+    .Task
+    .update({
+        text: req.body.text,
+        complete: req.body.complete
+    },{
+        where: {
+            id: req.body.id
+        }
+    }).then(function(dbTodo){
+        res.json(dbTodo);
+    });
+});
+
+};
+
